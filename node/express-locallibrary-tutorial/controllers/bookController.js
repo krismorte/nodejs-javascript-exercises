aa6613b77f9b939d4cdd20a1fb55365b7eb70027
Krisnamourt Filho <krisnamourt_ti@hotmail.com>
2018-05-02 16:50:08 -0300

    Add delete book

--- node/express-locallibrary-tutorial/controllers/bookController.js
+++ node/express-locallibrary-tutorial/controllers/bookController.js
@@ -164,12 +164,52 @@ exports.book_create_post = [
 
 // Display book delete form on GET.
 exports.book_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book delete GET');
+    async.parallel({
+        book: function(callback) {
+            Book.findById(req.params.id).exec(callback)
+        },
+        book_bookinstances: function(callback) {
+            BookInstance.find({ 'book': req.params.id }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.book==null) { // No results.
+            res.redirect('/catalog/books');
+        }
+        // Successful, so render.
+        res.render('book_delete', { title: 'Delete Book', book: results.book, book_bookinstances: results.book_bookinstances } );
+    });
 };
 
 // Handle book delete on POST.
 exports.book_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book delete POST');
+    async.parallel({
+        book: function(callback) {
+            Book.findById(req.params.id).exec(callback)
+        },
+        book_bookinstances: function(callback) {
+            BookInstance.find({ 'book': req.params.id }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        // Success
+        if (results.book_bookinstances.length > 0) {
+            // Author has books. Render in same way as for GET route.
+            res.render('book_delete', { title: 'Delete Book', book: results.book, book_bookinstances: results.book_bookinstances } );
+            return;
+        }
+        else {
+            // Author has no books. Delete object and redirect to the list of authors.
+            Book.findByIdAndRemove(req.body.bookid, function deleteBook(err) {
+                if (err) { return next(err); }
+                // Success - go to author list
+                res.redirect('/catalog/books')
+            })
+        }
+    });
+    
+
+
 };
 
 // Display book update form on GET.
